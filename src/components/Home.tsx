4207dd88b8077324c668c1e0e834b3c7eb26c07a
kranks40 <eng_peters20@yahoo.com>
2020-08-20 06:26:26 -0400

    Clean the app

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,6 +1,6 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
-import ExploreContainer from '../components/ExploreContainer';
+import Login from './Login';
 import './Home.css';
 
 const Home: React.FC = () => {
@@ -17,7 +17,7 @@ const Home: React.FC = () => {
             <IonTitle size="large">Blank</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <Login />
       </IonContent>
     </IonPage>
   );
